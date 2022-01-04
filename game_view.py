c72f5efe490178e08be40812f80e5e3b8ac9d5c5
user <grasjesdraste@yandex.ru>
2022-01-04 19:56:29 +0300

    Добавил меню на библиотеке pygame_menu

--- game_view.py
+++ game_view.py
@@ -2,10 +2,6 @@ import os
 import sys
 import pygame
 
-size = width, height = 700, 500
-screen = pygame.display.set_mode(size)
-screen.fill(pygame.Color('white'))
-
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -24,11 +20,10 @@ def load_image(name, colorkey=None):
 
 
 class Form(pygame.sprite.Sprite):
-    image = load_image("good_circle.png", -1)
 
-    def __init__(self):
+    def __init__(self, level):
         super().__init__(all_sprites)
-        self.image = Form.image
+        self.image = load_image(level + '.png', -1)
         self.image = pygame.transform.scale(self.image, (200, 200))
         self.rect = self.image.get_rect()
         self.rect.x = (width - self.rect.w) // 2
@@ -38,17 +33,14 @@ class Form(pygame.sprite.Sprite):
 
 
 class Cookie(pygame.sprite.Sprite):
-    image = load_image("universla_cookie.png")
-
     def __init__(self):
         super().__init__(all_sprites)
-        self.image = Cookie.image
+        self.image = load_image("universla_cookie.png")
         self.image = pygame.transform.scale(self.image, (300, 300))
         self.rect = self.image.get_rect()
         print(self.image.get_at((150, 150)))
         self.rect.x = (width - self.rect.w) // 2
         self.rect.y = (height - self.rect.h) // 2
-
 
 
 class Spot(pygame.sprite.Sprite):
@@ -61,38 +53,57 @@ class Spot(pygame.sprite.Sprite):
         self.rect.y = pos[1] - 2
 
     def update(self):
-        if not pygame.sprite.collide_mask(self, star_form):
+        if not pygame.sprite.collide_mask(self, form):
             print("Проиграл")
 
 
-all_sprites = pygame.sprite.Group()
-drawed = pygame.sprite.Group()
-running = True
-clock = pygame.time.Clock()
-cookie = Cookie()
-star_form = Form()
-all_poses = set()
-pygame.display.set_caption("PySquid")
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    press = pygame.mouse.get_pressed()
-    if press[0]:
-        pos = pygame.mouse.get_pos()
-        if pos not in all_poses:
-            all_poses.add(pos)
-            spot = Spot(pos)
-            drawed.add(spot)
-            drawed.update()
-            xs = [i[0] for i in all_poses]
-            ys = [i[1] for i in all_poses]
+size = width, height = 700, 500
+FPS = 60
 
-            if len(xs) > 544 and len(ys) > 544:
-                print('Победа')
-                print(len(xs), len(ys))
-    all_sprites.update()
-    screen.fill(pygame.Color('grey'))
-    all_sprites.draw(screen)
-    pygame.display.flip()
-pygame.quit()
+
+def game_run(level):
+    drawed = pygame.sprite.Group()
+    clock = pygame.time.Clock()
+
+    screen = pygame.display.set_mode(size)
+    screen.fill(pygame.Color('white'))
+    all_poses = set()
+    pygame.display.set_caption("PySquid")
+
+    global all_sprites
+    global cookie
+    global form
+
+    all_sprites = pygame.sprite.Group()
+    cookie = Cookie()
+    form = Form(level)
+
+    running = True
+    while running:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        press = pygame.mouse.get_pressed()
+        if press[0]:
+            pos = pygame.mouse.get_pos()
+            if pos not in all_poses:
+                all_poses.add(pos)
+                spot = Spot(pos)
+                drawed.add(spot)
+                drawed.update()
+                xs = [i[0] for i in all_poses]
+                ys = [i[1] for i in all_poses]
+
+                if len(xs) > 544 and len(ys) > 544:
+                    print('Победа')
+                    return
+        all_sprites.update()
+        screen.fill(pygame.Color('grey'))
+        all_sprites.draw(screen)
+        pygame.display.flip()
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    game_run('lightning')
