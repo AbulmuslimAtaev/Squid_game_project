0fda4cb30eeabc9a3d5ab8756f33ff116d6c1b65
ataev <ataevmuslim1995@yandex.ru>
2022-01-06 19:27:55 +0300

    victory system added

--- game_view.py
+++ game_view.py
@@ -1,14 +1,15 @@
 import os
 import sys
 import pygame
+from pillow_part import pic2text
 
 size = width, height = 700, 500
 screen = pygame.display.set_mode(size)
 screen.fill(pygame.Color('white'))
-
+IMAGE_NAME = "cloud"
 
 def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
+    fullname = os.path.join('images', name)
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
@@ -24,7 +25,7 @@ def load_image(name, colorkey=None):
 
 
 class Form(pygame.sprite.Sprite):
-    image = load_image("lightning.png", -1)
+    image = load_image(f"{IMAGE_NAME}.png", -1)
 
     def __init__(self):
         super().__init__(all_sprites)
@@ -50,7 +51,6 @@ class Cookie(pygame.sprite.Sprite):
         self.rect.y = (height - self.rect.h) // 2
 
 
-
 class Spot(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__(all_sprites)
@@ -65,9 +65,32 @@ class Spot(pygame.sprite.Sprite):
             print("Проиграл")
 
 
-
 all_sprites = pygame.sprite.Group()
+
+
+class Check_Form(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__(all_sprites)
+        self.image = pygame.Surface((5, 5), pygame.SRCALPHA)
+        # self.image.fill((0, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.x = (width - 200) // 2 + pos[0] * 4 - 1
+        self.rect.y = (height - 200) // 2 + pos[1] * 4 - 1
+
+
+def draw_update():
+    for i in drawed_check.sprites():
+        if not pygame.sprite.spritecollideany(i, drawed):
+            return False
+    return True
+
+
+drawed_check = pygame.sprite.Group()
+values = pic2text(IMAGE_NAME)
+for i in values:
+    drawed_check.add(Check_Form(i))
 drawed = pygame.sprite.Group()
+check_form = pygame.sprite.Group
 running = True
 clock = pygame.time.Clock()
 cookie = Cookie()
@@ -82,8 +105,13 @@ while running:
         pos = pygame.mouse.get_pos()
         drawed.add(Spot(pos))
         drawed.update()
+        if draw_update():
+            running = False
     all_sprites.update()
     screen.fill(pygame.Color('grey'))
     all_sprites.draw(screen)
+    drawed_check.draw(screen)
     pygame.display.flip()
+if not running:
+    print("Победа")
 pygame.quit()
---
+++ pillow_part.py
@@ -0,0 +1,18 @@
+def pic2text(imagename):
+    from PIL import Image
+
+    im = Image.open(f'images/{imagename}.png')
+    pixels = im.load()
+    _WIDTH = 50
+    _HEIGHT = 50
+    resized_img = im.resize((_WIDTH, _HEIGHT), Image.ANTIALIAS)
+    pixels = resized_img.load()
+    x, y = resized_img.size
+    print(x, y)
+    values = []
+    for i in range(x):
+        for j in range(y):
+            c = pixels[i, j]
+            if c[0] <= 100 and c[1] <= 100 and c[2] <= 100:
+                values.append((i, j))
+    return values
